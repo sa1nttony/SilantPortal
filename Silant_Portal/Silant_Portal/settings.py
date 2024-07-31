ec66d1913f10bc8b8a89047d3bb307634a5f0f7b
TonySaqnt <sa1ntholytony@yandex.ru>
2024-07-31 18:51:50 +0500

    Mega final commit

--- Silant_Portal/Silant_Portal/settings.py
+++ Silant_Portal/Silant_Portal/settings.py
@@ -118,8 +118,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/home'
-LOGOUT_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
